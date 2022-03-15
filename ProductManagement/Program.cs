5de03603ae93e4eee27cde74032e7bcb03611cc6
Aftab-1998 <alam41976@gmail.com>
2022-03-15 17:00:30 +0530

    [AFTAB] Add; Added The welcome msg

---
+++ ProductManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Product Review Management! \n");
+        }
+    }
+}
